9b7039a2aace9c4e8250458906a521c2b5dadd32
JingChao <jc.jingchao@qq.com>
2022-10-29 16:34:02 +0800

    create miniprogram

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,50 @@
+// custom-tab-bar/index.js
+Component({
+  data: {
+    color: "#515151",
+    selectedColor: "#DAA520",
+    backgroundColor: "#ffffff",
+    list: [
+      {
+        pagePath: "/pages/index/index",
+        text: "首页",
+        iconPath: "/images/tabbar/index.png",
+        selectedIconPath: "/images/tabbar/index-selected.png"
+      },
+      {
+        pagePath: "/pages/hospital/hospital",
+        text: "医院",
+        iconPath: "/images/tabbar/hospital.png",
+        selectedIconPath: "/images/tabbar/hospital-selected.png"
+      },
+      {
+        pagePath: "/pages/publish/publish",
+        bulge:true,
+        text: "发布",
+        iconPath: "/images/tabbar/dog.png",
+        selectedIconPath: "/images/tabbar/dog-selected.png"
+      },
+      {
+        pagePath: "/pages/popularization/popularization",
+        text: "科普",
+        iconPath: "/images/tabbar/popularization.png",
+        selectedIconPath: "/images/tabbar/popularization-selected.png"
+      },
+      {
+        pagePath: "/pages/me/me",
+        text: "我的",
+        iconPath: "/images/tabbar/me.png",
+        selectedIconPath: "/images/tabbar/me-selected.png"
+      },
+    ]
+  },
+  attached() {
+  },
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({url}) 
+    }
+  }
+})
---
+++ pages/logs/logs.js
@@ -0,0 +1,26 @@
+// logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    logs: []
+  },
+  onLoad() {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return {
+          date: util.formatTime(new Date(log)),
+          timeStamp: log
+        }
+      })
+    })
+  },
+
+  onShow: function () {
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 2
+      })
+    }
+  },
+})
