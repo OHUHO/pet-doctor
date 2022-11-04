c006ecd2626e82207457200932c0acb202a05240
JingChao <jc.jingchao@qq.com>
2022-11-04 20:31:24 +0800

    update

--- components/recommend/recommend.js
+++ components/recommend/recommend.js
@@ -11,7 +11,19 @@ Component({
    * 组件的初始数据
    */
   data: {
+    obj:{
+      title: '这是一个标题',
+      imgSrc:'url',
+      username: '用户名',
+      
+      content:'这是一段内容，这是一段内容，这是一段内容',
+      imgSrc:["/images/cat.png"],
+      videoSrc:"/images/cat.png",
+      
+      views:44,
+      
 
+    }
   },
 
   /**
