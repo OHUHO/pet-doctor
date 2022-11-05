0e9cb6164ab8302379032ef8e49f818fa6a1bcb1
JingChao <jc.jingchao@qq.com>
2022-11-05 17:09:45 +0800

    update

--- components/recommend/recommend.js
+++ components/recommend/recommend.js
@@ -11,19 +11,48 @@ Component({
    * 组件的初始数据
    */
   data: {
-    obj:{
-      title: '这是一个标题',
-      imgSrc:'url',
-      username: '用户名',
-      
-      content:'这是一段内容，这是一段内容，这是一段内容',
-      imgSrc:["/images/cat.png"],
-      videoSrc:"/images/cat.png",
-      
-      views:44,
-      
-
-    }
+    dataList:[
+      {
+        title: '这是一个标题',
+        imgSrc:'url',
+        username: '用户名',
+        
+        content:'这是一段内容，这是一段内容，这是一段内容',
+        imgSrc:["/images/cat.png"],
+        videoSrc:"/images/cat.png",
+        
+        views:44,
+        goods:120,
+  
+      },
+      {
+        title: '这是一个标题',
+        imgSrc:'url',
+        username: '用户名',
+        
+        content:'这是一段内容，这是一段内容，这是一段内容',
+        imgSrc:["/images/cat.png","/images/cat.png","/images/cat.png"],
+        videoSrc:"/images/cat.png",
+        
+        views:44,
+        goods:120,
+  
+      },
+      {
+        title: '这是一个标题',
+        imgSrc:'url',
+        username: '用户名',
+        
+        content:'这是一段内容，这是一段内容，这是一段内容',
+        imgSrc:[],
+        videoSrc:"/images/cat.png",
+        
+        views:44,
+        goods:120,
+  
+      }
+    ],
+    
   },
 
   /**
