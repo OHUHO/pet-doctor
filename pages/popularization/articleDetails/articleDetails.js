16ccac2f958834469969af3436cc95d1b9afe921
JingChao <jc.jingchao@qq.com>
2022-11-06 14:15:31 +0800

    update

--- pages/popularization/articleDetails/articleDetails.js
+++ pages/popularization/articleDetails/articleDetails.js
@@ -6,17 +6,18 @@ Page({
    */
   data: {
     article: {
-      title: '这是一个标题',
+      title: '电子科技大学成都学院这是一个文章的标题，这是标题？',
       imgSrc:'url',
       username: '用户名',
       portrait:'/images/be/6.jpeg',
 
-      content:'测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测',
+      content:'测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测这些全部是测试数据，这些展示的全部是测试数据测试数据。测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测这些全部是测试数据，这些展示的全部是测试数据测试数据。测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测这些全部是测试数据，这些展示的全部是测试数据测试数据。',
       imgSrc:["/images/cat.png"],
       videoSrc:"/images/cat.png",
       
       views:44,
       goods:120,
+      time:"2022-11-06 13:46"
 
     },
     temp:{
@@ -34,6 +35,36 @@ Page({
       "createTime":"2022-03-09 16:03:03",
       "contents":"电子科技大学成都学院计算机学院/商学院联合开发的微信小程序即将上线！！"
   },
+  comment:{
+    "num": 250,
+    "commentId": 23,
+    "messageId": 22,
+    "userId": "220118CK3F6DGSCH",
+    "username": "Aubuary",
+    "portrait": "https://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83eo0JnOqicrn63GG2AQAOTsuFRjV1ibnIlhBjnsYrHcHwfqoicm5PiaON6nEDUwL4R7xT3uY2c9TnhZDSA/132",
+    "creatTime": "2022-03-09 16:06:06",
+    "commentDetail": "扭转乾坤",
+    "replyList": [
+        {
+            "replyId": 34,
+            "commentId": 23,
+            "userId": "220118CK3F6DGSCH",
+            "username": "Aubuary",
+            "portrait": "https://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83eo0JnOqicrn63GG2AQAOTsuFRjV1ibnIlhBjnsYrHcHwfqoicm5PiaON6nEDUwL4R7xT3uY2c9TnhZDSA/132",
+            "creatTime": "2022-03-09 16:07:07",
+            "replyDetail": "冲冲冲！！！"
+        },
+        {
+            "replyId": 40,
+            "commentId": 23,
+            "userId": "220118CK3F6DGSCH",
+            "username": "Aubuary",
+            "portrait": "https://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83eo0JnOqicrn63GG2AQAOTsuFRjV1ibnIlhBjnsYrHcHwfqoicm5PiaON6nEDUwL4R7xT3uY2c9TnhZDSA/132",
+            "creatTime": "2022-11-06 11:06:06",
+            "replyDetail": "宠物医生来了"
+        }
+    ]
+}
 
   },
 
@@ -41,9 +72,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    this.setData({
-      article: options
-    })
+    // this.setData({
+    //   article: options
+    // })
     
   },
 
