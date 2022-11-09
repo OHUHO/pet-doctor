681c4beea17ca0829940ca30b1acf3a79543ce9b
JingChao <jc.jingchao@qq.com>
2022-11-09 12:35:20 +0800

    update

--- pages/hospital/hospital.js
+++ pages/hospital/hospital.js
@@ -5,8 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
+    isShowInput: false, //控制输入栏
 
   },
+
+  //点击出现输入框
+  showInput: function () {
+    this.setData({
+      isShowInput: true
+    })
+    console.error('show+++++++++++')
+  },
+
+  //隐藏输入框
+  onHideInput: function () {
+    this.setData({
+      showInput: false
+    })
+    console.error('hide+++++++++++')
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
