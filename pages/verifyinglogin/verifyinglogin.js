22470f2ba3bd2985881010c035917ad68885835f
JingChao <jc.jingchao@qq.com>
2022-11-15 13:41:59 +0800

    update

--- pages/verifyinglogin/verifyinglogin.js
+++ pages/verifyinglogin/verifyinglogin.js
@@ -7,60 +7,44 @@ Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  
+  //微信授权
+  bindGetUserInfo(e) {
+    if (e.detail.userInfo) { //是否授权
+      app.userInfo = e.detail.userInfo
+      // 获取code码
+      wx.login({
+        timeout: 10000,
+        success: (res) => {
+          this._getSessionKey(res.code)
+        }
+      });
+    } else {
+      console.log('用户没有授权')
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  // code码换取openid
+  _getSessionKey(code) {
+    const paramsObj = {
+      code: code,
+      channel: 1,
+      platfrom: 2,
+    }
+    app.api.login.getSessionKey(paramsObj)
+      .then(res => {
+        console.log(res)
+        if (res.code === 0) {
+          const data = res.data
+          wx.setStorageSync('openid', data.openid)
+          app.common.route.redirectTo({
+            url: app.route.login
+          })
+        }
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  // 暂不授权
+  onBackBtn() {
+    // app.common.route.navigateBack(1)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+  
 })
