35317d9d18eee7f8d0957e7a5f86870615de7bac
JingChao <jc.jingchao@qq.com>
2022-11-15 21:33:00 +0800

    update

--- pages/verifyinglogin/verifyinglogin.js
+++ pages/verifyinglogin/verifyinglogin.js
@@ -18,17 +18,17 @@ Page({
         wx.login({
           success (res) {
             console.log(res.code)
-            // if (res.code) {
-            //   //发起网络请求
-            //   wx.request({
-            //     url: 'https://example.com/onLogin',
-            //     data: {
-            //       code: res.code
-            //     }
-            //   })
-            // } else {
-            //   console.log('登录失败！' + res.errMsg)
-            // }
+            if (res.code) {
+              //发起网络请求
+              wx.request({
+                url: 'https://example.com/onLogin',
+                data: {
+                  code: res.code
+                }
+              })
+            } else {
+              console.log('登录失败！' + res.errMsg)
+            }
           }
         })
       }
